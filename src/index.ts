1a5bc0c2894f519ba373723b857e51b947778ed6
kgtkr <contact@kgtkr.net>
2021-11-04 00:17:11 +0900

    自動でサーバーをダウンロードするようにした

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { ExtensionContext } from "vscode";
 import * as vscode from "vscode";
-
+import * as os from "os";
 import {
   LanguageClient,
   LanguageClientOptions,
@@ -9,118 +9,286 @@ import * as path from "path";
 import { promises as fs } from "fs";
 import { ChildProcessWithoutNullStreams, spawn } from "child_process";
 import * as net from "net";
+import axios from "axios";
+import * as https from "https";
+import * as fsLegacy from "fs";
 
-let childProcess: ChildProcessWithoutNullStreams | null = null;
-let client: LanguageClient | null = null;
-const output = vscode.window.createOutputChannel("processing-language-server");
-
-async function cleanup() {
-  if (client !== null) {
-    await client.stop();
-    client = null;
-  }
-
-  if (childProcess !== null) {
-    childProcess.kill();
-    childProcess = null;
-  }
-}
-
-async function start(): Promise<void> {
-  await cleanup();
-  const config = vscode.workspace.getConfiguration(
+class ProcessingLanguageServerClient {
+  readonly logOutputConsole = vscode.window.createOutputChannel(
     "processing-language-server"
   );
-
-  const languageServerPath = config.get<string>("languageServerPath")!;
-  const processingPath = config.get<string>("processingPath")!;
-
-  let classpath = "";
-
-  const addJars = async (dir: string) => {
-    const absDir = path.join(processingPath, dir);
-    const names = await fs.readdir(absDir);
-    const jars = names
-      .filter((name) => name.endsWith(".jar"))
-      .map((name) => path.join(absDir, name));
-    for (const jar of jars) {
-      classpath += jar + ":";
-    }
-  };
-
-  await addJars("lib");
-  await addJars(path.join("core", "library"));
-  await addJars(path.join("modes", "java", "mode"));
-  classpath += languageServerPath;
-
-  const clientOptions: LanguageClientOptions = {
-    documentSelector: [{ scheme: "file", language: "processing" }],
-    synchronize: {
-      fileEvents: vscode.workspace.createFileSystemWatcher("**/*.pde"),
-    },
-  };
-
-  client = new LanguageClient(
-    "processing-language-server",
-    "ProcessingLanguageServer",
-    async () => {
-      const port = Math.floor(Math.random() * (65535 - 1024) + 1024);
-      let ready = false;
-
-      output.appendLine(`Starting language server. Port: ${port}`);
-
-      await new Promise((resolve) => {
-        childProcess = spawn(path.join(processingPath, "java", "bin", "java"), [
-          "-Djna.nosys=true",
-          "-classpath",
-          classpath,
-          "net.kgtkr.processingLanguageServer.main",
-          String(port),
-        ]);
-
-        childProcess.stdout.on("data", (data) => {
-          output.appendLine("[stdout]" + data.toString());
-
-          if (!ready && data.toString().includes("Ready")) {
-            ready = true;
-            resolve(undefined);
-          }
-        });
-
-        childProcess.stderr.on("data", (data) => {
-          output.appendLine("[stderr]" + data.toString());
-        });
-
-        childProcess.on("close", () => {
-          output.appendLine("[close]");
-        });
-      });
-
-      const conn = net.connect({ port });
-      return {
-        reader: conn,
-        writer: conn,
-      };
-    },
-    clientOptions
+  languageServerProcess: ChildProcessWithoutNullStreams | null = null;
+  languageClient: LanguageClient | null = null;
+  languageServerConn: net.Socket | null = null;
+  readonly config = vscode.workspace.getConfiguration(
+    "processing-language-server"
   );
+  processingVersion = "";
 
-  client.start();
+  async loadProcessingVersion() {
+    const processingPath = this.config.get<string>("processingPath")!;
+    try {
+      this.processingVersion = await fs.readFile(
+        path.join(processingPath, "lib", "version.txt"),
+        { encoding: "utf-8" }
+      );
+    } catch {
+      await vscode.window.showErrorMessage(
+        `Processing path: '${processingPath}' is invalid. Please set the correct path to 'processing-language-server.processingPath' and reload.`
+      );
+    }
+  }
+
+  async updateLanguageServer() {
+    const autoUpdate = this.config.get<boolean>("autoUpdate")!;
+    if (!autoUpdate) {
+      return;
+    }
+
+    const languageServerDir = path.join(
+      os.homedir(),
+      "processing-language-server"
+    );
+
+    try {
+      await fs.mkdir(languageServerDir);
+    } catch {}
+
+    const installedVersions = new Set(
+      (await fs.readdir(languageServerDir))
+        .filter((x) => x.endsWith(".jar"))
+        .map((x) => x.substring(0, x.length - 4))
+    );
+
+    let remoteVersions: string[];
+    try {
+      remoteVersions = await axios
+        .get(
+          "https://api.github.com/repos/kgtkr/processing-language-server/git/trees/bin"
+        )
+        .then((res) => res.data)
+        .then((json: { tree: Array<{ path: string }> }) =>
+          json.tree
+            .map((x) => x.path)
+            .filter((x) => x.endsWith(".jar"))
+            .map((x) => x.substring(0, x.length - 4))
+        );
+    } catch {
+      return;
+    }
+
+    const filteredRemoteVersions = remoteVersions
+      .filter((x) => {
+        const allowVersion = x.split("-")[0];
+        return (
+          this.processingVersion === allowVersion ||
+          this.processingVersion.startsWith(allowVersion + ".")
+        );
+      })
+      .sort((a, b) => Number(b.split("-")[1]) - Number(a.split("-")[1]));
+
+    const newVersion: string | undefined = filteredRemoteVersions[0];
+
+    if (newVersion === undefined) {
+      await vscode.window.showErrorMessage(
+        `Processing version: ${this.processingVersion} is not supported. Maybe it works if you install it manually.`
+      );
+      return;
+    }
+
+    if (installedVersions.has(newVersion)) {
+      return;
+    }
+
+    const newVersionPath = path.join(languageServerDir, `${newVersion}.jar`);
+    const newVersionUrl = `https://github.com/kgtkr/processing-language-server/raw/bin/${newVersion}.jar`;
+
+    try {
+      const tmp = path.join(
+        await fs.mkdtemp(path.join(os.tmpdir(), "processing-language-server-")),
+        "processing-language-server.jar"
+      );
+
+      await vscode.window.withProgress(
+        {
+          location: vscode.ProgressLocation.Notification,
+          title: "Updating Processing Language Server",
+          cancellable: false,
+        },
+        async (progress) => {
+          await new Promise((resolve, reject) => {
+            https.get(newVersionUrl, (res) => {
+              let downloadSize = 0;
+              const file = fsLegacy.createWriteStream(tmp);
+              res.pipe(file);
+              file.on("finish", async () => {
+                resolve(undefined);
+              });
+              res.on("data", (chunk: Buffer) => {
+                downloadSize += chunk.byteLength;
+                progress.report({
+                  message: `${downloadSize / 1000}KB`,
+                });
+              });
+              res.on("error", (e) => {
+                reject(e);
+              });
+            });
+          });
+        }
+      );
+
+      await fs.rename(tmp, newVersionPath);
+
+      await fs.writeFile(
+        newVersionPath,
+        await axios(newVersionUrl, {
+          responseType: "arraybuffer",
+          onDownloadProgress: (progress) => {
+            this.logOutputConsole.append(
+              `Downloading ${newVersion}... ${Math.round(progress.percent)}%`
+            );
+          },
+        })
+          .then((res): ArrayBuffer => res.data)
+          .then((data) => Buffer.from(data))
+      );
+
+      await this.config.update(
+        "languageServerPath",
+        newVersionPath,
+        vscode.ConfigurationTarget.Global
+      );
+
+      await vscode.window.showInformationMessage(
+        `Processing Language Server is updated.`
+      );
+    } catch (e) {
+      await vscode.window.showErrorMessage(
+        `Failed to update Processing Language Server: ${e}`
+      );
+    }
+  }
+
+  async languageServerStop() {
+    if (this.languageClient !== null) {
+      await this.languageClient.stop();
+      this.languageClient = null;
+    }
+
+    if (this.languageServerConn !== null) {
+      this.languageServerConn.destroy();
+      this.languageServerConn = null;
+    }
+
+    if (this.languageServerProcess !== null) {
+      this.languageServerProcess.kill();
+      this.languageServerProcess = null;
+    }
+  }
+
+  async languageServerStart() {
+    await this.languageServerStop();
+
+    const languageServerPath = this.config.get<string>("languageServerPath")!;
+    const processingPath = this.config.get<string>("processingPath")!;
+
+    let classpath = "";
+
+    const addJars = async (dir: string) => {
+      const absDir = path.join(processingPath, dir);
+      const names = await fs.readdir(absDir);
+      const jars = names
+        .filter((name) => name.endsWith(".jar"))
+        .map((name) => path.join(absDir, name));
+      for (const jar of jars) {
+        classpath += jar + ":";
+      }
+    };
+
+    await addJars("lib");
+    await addJars(path.join("core", "library"));
+    await addJars(path.join("modes", "java", "mode"));
+    classpath += languageServerPath;
+
+    const clientOptions: LanguageClientOptions = {
+      documentSelector: [{ scheme: "file", language: "processing" }],
+      synchronize: {
+        fileEvents: vscode.workspace.createFileSystemWatcher("**/*.pde"),
+      },
+    };
+
+    this.languageClient = new LanguageClient(
+      "processing-language-server",
+      "ProcessingLanguageServer",
+      async () => {
+        const port = Math.floor(Math.random() * (65535 - 1024) + 1024);
+        let ready = false;
+
+        this.logOutputConsole.appendLine(
+          `Starting language server. Port: ${port}`
+        );
+
+        await new Promise((resolve) => {
+          this.languageServerProcess = spawn(
+            path.join(processingPath, "java", "bin", "java"),
+            [
+              "-Djna.nosys=true",
+              "-classpath",
+              classpath,
+              "net.kgtkr.processingLanguageServer.main",
+              String(port),
+            ]
+          );
+
+          this.languageServerProcess.stdout.on("data", (data) => {
+            this.logOutputConsole.appendLine("[stdout]" + data.toString());
+
+            if (!ready && data.toString().includes("Ready")) {
+              ready = true;
+              resolve(undefined);
+            }
+          });
+
+          this.languageServerProcess.stderr.on("data", (data) => {
+            this.logOutputConsole.appendLine("[stderr]" + data.toString());
+          });
+
+          this.languageServerProcess.on("close", () => {
+            this.logOutputConsole.appendLine("[close]");
+          });
+        });
+
+        this.languageServerConn = net.connect({ port });
+        return {
+          reader: this.languageServerConn,
+          writer: this.languageServerConn,
+        };
+      },
+      clientOptions
+    );
+
+    this.languageClient.start();
+  }
 }
 
+const client = new ProcessingLanguageServerClient();
+
 export async function activate(context: ExtensionContext): Promise<void> {
+  await client.loadProcessingVersion();
+  await client.updateLanguageServer();
   context.subscriptions.push(
     vscode.commands.registerCommand(
       "processing-language-server.restart",
       () => {
-        start();
+        client.languageServerStart();
       }
     )
   );
 
-  await start();
+  await client.languageServerStart();
 }
 
 export async function deactivate(): Promise<void> {
-  await cleanup();
+  await client.languageServerStop();
 }
